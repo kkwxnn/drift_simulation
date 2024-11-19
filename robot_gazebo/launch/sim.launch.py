be023a58626601aa15442c67bd3adc9d2d75dee8
Monsicha Sopitlaptana <122891621+kkwxnn@users.noreply.github.com>
2024-11-19 17:50:13 +0700

    change to effort control

--- robot_gazebo/launch/sim.launch.py
+++ robot_gazebo/launch/sim.launch.py
@@ -69,7 +69,7 @@ def generate_launch_description():
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["velocity_controllers", "--controller-manager", "/controller_manager"],
+        arguments=["effort_controllers", "--controller-manager", "/controller_manager"],
     )
 
     launch_description = LaunchDescription()
